f7c3aa7acb3b557e9c9209ebb16b1859415904f1
Aldon Manuel <ald19.mnl04@gmail.com>
2025-01-16 03:48:27 +0000

    test

--- models/example/user_astrapay.sql
+++
@@ -1,5 +0,0 @@
-SELECT 
-id, name, place_of_birth, date_of_birth, nationality, gender, profession, 
-country, province, city, district, village, address, rt_number, rw_number, postal_code, 
-created_at, updated_at
-FROM `fiery-emblem-447702-a9.dbt_amanuel.user`
